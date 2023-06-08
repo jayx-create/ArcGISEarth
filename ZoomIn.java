dcfb6a723c6a5b68d6eea99aa4fe1f2484451559
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-06-08 15:55:19 -0700

    Update ZoomIn.java

--- ZoomIn.java
+++ ZoomIn.java
@@ -2,3 +2,10 @@ import io.cucumer.java.en.And;
 import io.cucumer.java.en.Given;
 import io.cucumer.java.en.Then;
 import io.cucumer.java.en.When;
+
+public class ZoomIn {
+  
+@Then("User will be able to zoom in")
+  public void userWillBeAbleToZoomIn(){
+    
+  }
