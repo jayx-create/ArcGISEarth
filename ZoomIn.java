9d6ecdf3ca382e2148ab85fedafb17c3fc5f4f5e
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-06-08 16:22:19 -0700

    Update ZoomIn.java

--- ZoomIn.java
+++ ZoomIn.java
@@ -7,5 +7,24 @@ public class ZoomIn {
   
 @Then("User will be able to zoom in")
   public void userWillBeAbleToZoomIn(){
-    
+    MobileElement elementToZoom = driver.findElement(MobileBy.id("elementId"));
+        Dimension size = elementToZoom.getSize();
+        int aStart = elementToZoom.getLocation().getX() + size.getWidth() / 2;
+        int bStart = elementToZoom.getLocation().getY() + size.getHeight() / 2;
+        int finishA = aStart - 100;
+        int finishB = bStart - 100;
+        MultiTouchAction multiTouch = new MultiTouchAction(driver);
+        multiTouch.add(new TouchAction<>(driver)
+                .press(elementToZoom, aStart, bStart)
+                .moveTo(elementToZoom, finishA, finishB)
+                .release())
+                .add(new TouchAction<>(driver)
+                        .press(elementToZoom, aStart, bStart)
+                        .moveTo(elementToZoom, finishA, finishB)
+                        .release());
+
+        multiTouch.perform();
+        driver.quit();
   }
+
+}
