cff67a83e20b5e8a817b14aa4e50d52e0832de0d
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-06-08 15:41:47 -0700

    Update Zoomin.java

--- Zoomin.java
+++ Zoomin.java
@@ -6,9 +6,21 @@ import io.cucumer.java.en.When;
 public class Zoomin {
   
   
-@Given(
+@Given("{string} is logged in")
+  public void loginProfileIdIsLoggedIn(String loginProfileId) throws Exception{
+   baseUtil.userInformation = TestUserUtil.getTestUserByProfileId(loginProfileId);
+   realData = baseutil.userInformation;
+   name = realData.getUserName();
+   switch(deviceMode.toUpperCase()) {
+     case GlobalConstant.PHONE:
+       if ( expectedLanguage.equals("English"){
+         mobileEarthGISArc.invoke(username);
+         mobileEarthGISArc.waitForPageToLoad();
+       }
+   }
+ }           
 @When ("User sees earth on page")
-public void userSeesEarthOnPage(){
+  public void userSeesEarthOnPage(){
   switch(deviceMode) {
     case GlobalConstant.PHONE:
        switch(language) {
@@ -18,3 +30,15 @@ public void userSeesEarthOnPage(){
     }
   }
 }
+           
+@And ("User is registered with {string}")
+     public void userIsRegisteredWithLanguage(String language) throws Exception{
+  if(userLanguage. equalsIgnoreCase (GlobalConstant.ENGLISH)) {
+baseutil.deviceProp.setPreferredLanguage(GlobalConstant.ENGLISH);
+if(!dataType.toUpperCase().contains (GlobalConstant.DATA_TYPE_VIRTUAL)) {
+changeUserPreferredLanguage(GlobalConstant.ENGLISH_PREFFERED_LANG_CODE)
+  }
+  }
+     }
+           
+           
