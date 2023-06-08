f03b022e227c07c3d5fe6f35349a50634c79f791
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-06-08 15:19:26 -0700

    Update Zoomin.java

--- Zoomin.java
+++ Zoomin.java
@@ -2,3 +2,15 @@ import io.cucumer.java.en.And;
 import io.cucumer.java.en.Given;
 import io.cucumer.java.en.Then;
 import io.cucumer.java.en.When;
+
+public class Zoomin {
+  
+@Then ("User will be able to zoom in")
+public void userWillBeAbleToZoomIn(){
+  switch(deviceMode) {
+    case GlobalConstant.PHONE:
+       switch(language) {
+         case GlobalConstant.ENGLISH:
+           
+  }
+}
